f015ba42d6fdb6ec7a97614cee7efdda3a2f7898
moshushi <rin@MUSPELHEIM.olko.local>
2013-08-19 17:22:52 +0300

    Some try

--- filmdb.py
+++ filmdb.py
@@ -6,5 +6,17 @@ cursor = conn.cursor()
 
 #create a table
 cursor.execute("""CREATE TABLE films
-                (id integer, title text, release_date integer, format text, stars text)
+                (id integer, title text, release_year integer, format text, stars text)
                """)
+
+#insert some data
+cursor.execute ("INSERT INTO films VALUES (0001, 'Blazing Saddles', 1974, 'VHS', 'Mel Brooks')")
+
+#save data to database
+conn.commit ()
+
+#insert multiple records
+films =[(0002, 'Role', 1982, 'VDS', 'Some'),
+        (0003, 'Role 2', 1983, 'VDS', 'Some')]
+cursor.executemany("INSERT INTO films VALUES(?,?)", films)
+conn.commit
