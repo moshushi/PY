a3fcd10009f58f2b8f9d16dccc0cec941111d0de
moshushi <rin@MUSPELHEIM.olko.local>
2013-09-12 17:37:28 +0300

    2 source code db

--- filmdb.py
+++ filmdb.py
@@ -4,16 +4,16 @@ import sqlite3
 conn = sqlite3.connect("filmbase.db")
 #
 #open cursor
-cursor = con.cursor()
-#create tables
-cursor.execute('CREATE TABLE IF NOT EXISTS formats (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, format VARCHAR(7))')
-con.commit
-cursor.execute('CREATE TABLE IF NOT EXISTS films (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, title VARCHAR(30), yearrelease INTEGER)')
-con.commit
-cursor.execute('CREATE TABLE IF NOT EXISTS stars (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, firstname VARCHAR(30), secondname VARCHAR(30))')
-con.commit
-cursor.execute('CREATE TABLE IF NOT EXISTS film_star (films_id INTEGER NOT NULL, stars_id INTEGER NOT NULL, PRIMARY KEY (film_id, star_id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id))')
-con.commit
+c = conn.cursor()
+#create tables Section
+c.execute('''CREATE TABLE formats (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, format VARCHAR(7))''')
+conn.commit()
+c.execute('''CREATE TABLE films (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, title VARCHAR(30), yearrelease INTEGER)''')
+conn.commit()
+c.execute('''CREATE TABLE stars (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, firstname VARCHAR(30), secondname VARCHAR(30))''')
+conn.commit()
+c.execute('''CREATE TABLE film_star (films_id INTEGER NOT NULL, stars_id INTEGER NOT NULL, PRIMARY KEY (film_id, star_id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id))''')
+conn.commit()
 #
 #insert some data
 #cursor.execute ("INSERT INTO films VALUES (0001, 'Blazing Saddles', 1974, 'VHS', 'Mel Brooks')")
@@ -25,4 +25,4 @@ con.commit
 #films =[(0002, 'Role', 1982, 'VDS', 'Some'),
 #        (0003, 'Role 2', 1983, 'VDS', 'Some')]
 #cursor.executemany("INSERT INTO films VALUES(?,?)", films)
-#conn.commit
+conn.close()
