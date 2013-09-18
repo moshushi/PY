8befd2ce84f8f57ceca4a76b0286d8c1d812a109
moshushi <rin@MUSPELHEIM.olko.local>
2013-09-18 19:19:17 +0300

    Correct create first table

--- filmdb.py
+++ filmdb.py
@@ -5,16 +5,18 @@ conn = sqlite3.connect("filmbase.db")
 #
 #open cursor
 c = conn.cursor()
+#enable support foreign keys
+c.execute('PRAGMA foreign_keys = ON')
+#
 #create tables Section
-PRAGMA foreign_keys=ON
-c.execute('''CREATE TABLE formats (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, format VARCHAR(7))''')
+c.execute('''CREATE TABLE IF NOT EXISTS formats (id INTEGER PRIMARY KEY AUTOINCREMENT, format VARCHAR(7))''')
 conn.commit()
-c.execute('''CREATE TABLE films (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, title VARCHAR(30), yearrelease INTEGER)''')
+c.execute('''CREATE TABLE IF NOT EXISTS films (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(30), yearrelease INTEGER)''')
 conn.commit()
-c.execute('''CREATE TABLE stars (id INTEGER PRIMARY KEY AUTOINCREMENT NO NULL, firstname VARCHAR(30), secondname VARCHAR(30))''')
+c.execute('''CREATE TABLE IF NOT EXISTS stars (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(30), secondname VARCHAR(30))''')
 conn.commit()
-c.execute('''CREATE TABLE film_star (films_id INTEGER NOT NULL, stars_id INTEGER NOT NULL, PRIMARY KEY (film_id, star_id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id))''')
-conn.commit()
+#c.execute('''CREATE TABLE film_star (films_id INTEGER NOT NULL, stars_id INTEGER NOT NULL, PRIMARY KEY (film_id, star_id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id), CONSTRAINT film_id_fk FOREIGN KEY (film_id) REFERENCES film(id))''')
+#conn.commit()
 #
 #insert some data
 #cursor.execute ("INSERT INTO films VALUES (0001, 'Blazing Saddles', 1974, 'VHS', 'Mel Brooks')")
