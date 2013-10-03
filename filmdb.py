a6f443ea8e0ceab956785d6d1184f79fda938ef7
moshushi <rin@MUSPELHEIM.olko.local>
2013-10-03 17:29:38 +0300

    dont't work dictinary

--- filmdb.py
+++ filmdb.py
@@ -37,12 +37,19 @@ def inst_db():
 #
 #insert multiple records
 #films =[(0002, 'Role', 1982, 'VDS', 'Some'),
-#        (0003, 'Role 2', 1983, 'VDS', 'Some')]
+# (0003, 'Role 2', 1983, 'VDS', 'Some')]
 #cursor.executemany("INSERT INTO films VALUES(?,?)", films)
+    #conn.close()
+def inst_pl_db():
+    dict = {"id":NULL, "title":'Blazing Saddles', "yearrelaease":1974}
+    conn = sqlite3.connect("filmbase.db")
+    c = conn.cursor()
+#    c.execute ("INSERT INTO stocks (id, title, yearrelease) VALUES (?,?,?)", [dict["id"], dict["titls"], dict["yearrelease"]
+    conn.commit()
     conn.close()
 def main():
     make_db()
-    inst_db()
+    inst_pl_db()
 
 if __name__ == "__main__":
     main()
