1ee930e286daea65a85f81d961c8d7bbec64f8c3
lis <lis@lis-laptop.(none)>
2013-11-19 23:42:04 +0200

    change inout.py with global variables

--- inout.py
+++ inout.py
@@ -1,3 +1,14 @@
 # -*- coding: cp1251 -*-
-var = raw_input("Enter something: ")
-print "you entered ", var
+globavar = 0
+
+def in_row():
+    global globalvar
+    globalvar = raw_input("Enter something: ")
+def out_row():
+    print ("you entered ", globalvar)
+def main():
+    in_row()
+    out_row()
+
+if __name__ ==  "__main__":
+    main()
