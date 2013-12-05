2a5ebde087adfda61575a4d6d11b381b7a7ea427
moshushi <moshushi@gmail.com>
2013-12-06 00:30:42 +0200

    change multichoise

--- multichoise.py
+++ multichoise.py
@@ -1,7 +1,21 @@
 # -*- coding: cp1251 -*-
+import sqlite3
 
-tr = raw_input("Choise VHS, DVD or Blu-Ray")
-while choice not in ["VHS", "DVD", "Blu-Ray"]:
-    tr = raw_input("Choise VHS, DVD or Blu-Ray")
+def make_form_db():
+    conn = sqlite3.connect('filmbase.db')
+    c = conn.cursor()
+    #c.execute('''CREATE TABLE IF NOT EXISTS formatsmovie (id INTEGER PRIMARY KEY, format VARCHAR(7) XXXXXXXX)''')
+    conn.commit()
+    conn.close()
 
+def informat():
+    tr = raw_input("Choise VHS, DVD or Blu-Ray ")
+    while tr not in ["VHS", "DVD", "Blu-Ray"]:
+        tr = raw_input("Choise VHS, DVD or Blu-Ray")
+    print (tr)
 
+def main():
+        informat()
+
+if __name__ == "__main__":
+    main()
