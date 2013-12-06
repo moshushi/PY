93acf03a0c502729dda8bb17368cf8a0a4f0d18e
moshushi <rin@MUSPELHEIM.olko.local>
2013-12-06 11:01:32 +0200

    add table formatm

--- multichoise.py
+++ multichoise.py
@@ -1,12 +1,18 @@
 # -*- coding: cp1251 -*-
 import sqlite3
 
-def make_form_db():
+global tr
+
+def createtable_formatmovie():
     conn = sqlite3.connect('filmbase.db')
     c = conn.cursor()
+    c.execute('PRAGMA foreign_keys = ON')
+    c.execute('''CREATE TABLE IF NOT EXISTS formatm (film_id INTEGER, formats VARCHAR(8) NOT NULL CHECK (formats IN ('VHS', 'DVD', 'Blu-Ray')), FOREIGN KEY(film_id) REFERENCES films(id))''')
     #c.execute('''CREATE TABLE IF NOT EXISTS formatsmovie (id INTEGER PRIMARY KEY, format VARCHAR(7) XXXXXXXX)''')
     conn.commit()
     conn.close()
+
+#def choise_formatmovie():
 
 def informat():
     tr = raw_input("Choise VHS, DVD or Blu-Ray\n")
@@ -15,7 +21,8 @@ def informat():
     print (tr)
 
 def main():
-        informat()
+    informat()
+    createtable_formatmovie()
 
 if __name__ == "__main__":
     main()
