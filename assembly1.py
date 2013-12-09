966371292af0d2a959dec4ef4bd497aa5ee0d39f
moshushi <rin@MUSPELHEIM.olko.local>
2013-12-09 14:10:02 +0200

    touch

---
+++ assembly1.py
@@ -0,0 +1,22 @@
+# -*- coding: cp1251 -*-
+import sqlite3
+
+def make_db():
+    conn = sqlite3.connect('filmbase.db')
+    c = conn.cursor()
+    c.execute('PRAGMA foreign_keys = ON')
+    c.execute('''CREATE TABLE IF NOT EXISTS films (id INTEGER PRIMARY KEY, title VARCHAR(30), yearrelease INTEGER)''')
+    conn.commit()
+    c.execute('''CREATE TABLE IF NOT EXISTS stars (id INTEGER PRIMARY KEY, name VARCHAR(60) UNIQUE)''')
+    conn.commit()
+    c.execute('''CREATE TABLE IF NOT EXISTS film_star (film_id INTEGER, star_id INTEGER, FOREIGN KEY(film_id) REFERENCES films(id), FOREIGN KEY(star_id) REFERENCES stars(id)) ''')
+    conn.commit()
+    c.execute('''CREATE TABLE IF NOT EXISTS formatm (film_id INTEGER, formats VARCHAR(8) NOT NULL CHECK (formats IN ('VHS', 'DVD', 'Blu-Ray')), FOREIGN KEY(film_id) REFERENCES films(id))''')
+    conn.commit()
+    conn.close()
+
+def main():
+    make_db()
+
+if __name__ == "__main__":
+    main()
