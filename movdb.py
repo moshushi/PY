14be928980318973a1a852aa52f84ca9ea268b3d
moshushi <moshushi@gmail.com>
2015-02-19 12:21:55 +0200

    add add-title

--- movdb.py
+++ movdb.py
@@ -1,8 +1,9 @@
 # -*- coding: cp1251 -*-
-import sqlite3
 """Study task movie database film application
 """
+import sqlite3
 
+global conn
 
 #class Movie(object):
 #    def __init__(self, id, title, ryear, format, stars)
@@ -11,14 +12,18 @@ import sqlite3
 #    pass
 
 class Movie(object):
+    #def __init__(self):
     def __init__(self, title):
    # def __init__(self, id, title):
     #    self.id = id
         self.title = title
 
-    def save(self, title):
-        c.execute('INSERT OR IGNORE INTO films(title) VALUES (?)', (title))
-
+    #def save(self, title):
+    def save(self):
+        global conn
+        c = conn.cursor()
+        c.execute('INSERT OR IGNORE INTO films(title) VALUES (?)', (self.title,))
+        conn.commit()
 #    def add_new(self, title):
 
 #?add new, search?
@@ -46,18 +51,22 @@ class UI(object):
             UI.user_input(self)
         elif UI.ch == 'quit':
             UI.quit(self)
+        elif UI.ch == 'add':
+            UI.add_movie(self)
         else:
             UI.start(self)
 
     def add_movie(self):
         pass
-        f = Movie()
-        f.title = raw_input("Enter movie title:")
+        f = Movie('Null')
+        f.title = raw_input("Enter movie title=>")
         #f.movie()
-        #f.save()
+        print f.title
+        f.save()
 
     def help(self):
         print ("help")
+        UI.user_input(self)
 
     def quit(self):
         #conn.commit()
@@ -65,13 +74,16 @@ class UI(object):
         pass
 
 def conn_or_create_db():
-    conn = sqlite3.connect('filmbase.db')
+#    conn = sqlite3.connect('filmbase.db')
+    global conn
 
     c = conn.cursor()
     c.execute('PRAGMA foreign_keys = ON')
     c.execute('''CREATE TABLE IF NOT EXISTS films (id INTEGER PRIMARY KEY, title VARCHAR(30), yearrelease INTEGER)''')
     conn.commit()
 
+
+conn = sqlite3.connect('filmbase.db')
 conn_or_create_db()
 a = UI()
 a.start()
