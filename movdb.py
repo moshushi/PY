7f5899718e8d63037c12950fb6bd5930fc272d46
moshushi <moshushi@gmail.com>
2015-02-19 10:53:37 +0200

    add choice

--- movdb.py
+++ movdb.py
@@ -11,14 +11,16 @@ import sqlite3
 #    pass
 
 class Movie(object):
-    def __init__(self, id, title):
-    #def __init__(self):
-    #    pass
-        self.id = id
+    def __init__(self, title):
+   # def __init__(self, id, title):
+    #    self.id = id
         self.title = title
 
-    def save(self):
-        pass
+    def save(self, title):
+        c.execute('INSERT OR IGNORE INTO films(title) VALUES (?)', (title))
+
+#    def add_new(self, title):
+
 #?add new, search?
 
 class UI(object):
@@ -26,18 +28,34 @@ class UI(object):
     def __init__(self):
         pass
 
+    ch = ''
+
     def start(self):
-   #     pass
         print ("Welcome to CMDB - Command line Movie Database.")
         print ("Type 'help' to see available commands.")
+        UI.user_input(self)
+
+    def user_input(self):
+        UI.ch = raw_input("=>")
+        UI.choice(self)
+
+    def choice(self):
+        if UI.ch == 'help':
+            UI.help(self)
+        elif UI.ch == '':
+            UI.user_input(self)
+        else:
+            UI.start(self)
 
     def add_movie(self):
         pass
+        f = Movie()
+        f.title = raw_input("Enter movie title:")
         #f.movie()
         #f.save()
 
     def help(self):
-        pass
+        print ("help")
 
 def conn_or_create_db():
     conn = sqlite3.connect('filmbase.db')
