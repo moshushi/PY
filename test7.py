62bd0bce4f94984d7bd0914814e1bcd64cacb937
moshushi <moshushi@gmail.com>
2015-03-11 11:59:03 +0200

    cleaning

--- test7.py
+++ test7.py
@@ -2,17 +2,23 @@
 ##while myl:
 ##    z = myl.pop(0)
 ##    print z
+def test_print():
+    z = [1,2,3,4]
+    while z:
+        x = z.pop(0)
+        print "x now is %s" % x
+        print "z now is %s" % z
 
-z = [1,2,3,4]
-while z:
-    x = z.pop(0)
-    print "x now is %s" % x
-    print "z now is %s" % z
 
+    z = [{1:2}, {3:4},]
+    print "new z now is %s" % z
+    while z:
+        x = z.pop(0)
+        print "x now is %s" % x
+        print "z now is %s" % z
 
-z = [{1:2}, {3:4},]
-print "new z now is %s" % z
-while z:
-    x = z.pop(0)
-    print "x now is %s" % x
-    print "z now is %s" % z
+def main():
+    test_print()
+
+if __name__ == "__main__":
+    main()
