ed47a94e9135b5449df3b35972ed15791a76c287
moshushi <moshushi@gmail.com>
2015-08-04 17:17:09 +0300

    repeat words

---
+++ test tasks/repeat words.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Programm repeat word, if word have 'a' - repeat 2 time, if 'z' - 3 time
+"""
+print "Enter the word. For end program enter 'Enter'"
+loop = 1
+
+while loop == 1:
+    word = raw_input("=>")
+    if word == '':
+        loop = 0    
+    else:
+        if 'z' in word:
+            print word + " " + word + " " + word
+        elif 'a' in word:
+            print word + " " + word
+        else:
+            print word
