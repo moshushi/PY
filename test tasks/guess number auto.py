192bc95e75fbc700ce7c920d6b2cf0c32fc748a5
moshushi <moshushi@gmail.com>
2015-08-11 17:51:50 +0300

    guess number auto

--- test tasks/guess number auto.py
+++ test tasks/guess number auto.py
@@ -3,17 +3,22 @@
 Guess number in 10 try
 """
 
-import random
+import random, math
 global guess_number
 
 i_try = 0
 guess_number = 0
 AMT_GUESS = 10
 
+guess_start = 0
+guess_finish = 100
+
 secret_number = random.randrange(1,101)
 
 while True:
-    guess_number = int(raw_input("Input guess number => "))
+    guess_number = int(math.ceil((guess_finish - guess_start)/2)) + guess_start
+    print 'computer choose number ', guess_number
+
     i_try += 1
     if guess_number == secret_number:
         print "You Win!!! You'r score is %s" % (AMT_GUESS - i_try)
@@ -26,5 +31,7 @@ while True:
     
     if guess_number > secret_number:
         print "too much"
+        guess_finish = guess_number
     else:
         print "too few"
+        guess_start = guess_number
