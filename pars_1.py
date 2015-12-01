fc20aa3f76e7ddac44d1c204a64a4f504c7b4160
moshushi <moshushi@gmail.com>
2015-12-01 17:37:57 +0200

    parsing

---
+++ pars_1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Example from http://youtu.be/KPXPr-KS-qk
+"""
+
+import urllib2
+from bs4 import BeautifulSoup
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
