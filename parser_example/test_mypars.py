700f9aa8bb941de2bc94d7f9d1272e9a19724642
moshushi <moshushi@gmail.com>
2016-05-23 18:49:56 +0300

    new

--- parser_example/test_mypars.py
+++ parser_example/test_mypars.py
@@ -3,17 +3,17 @@
 Unittests for webparser21
 """
 
-import unittest
-from mock import patch, Mock
+import unittest, sys
+from mock import patch, Mock, mock_open
 import webparser21 as wpars
 
 class TestDownl(unittest.TestCase):
 
-    @patch(__name__+".get_html", Mock())
+    @patch("webparser21.get_html", Mock())
     def _test_get_html(self):
-        get_html(BASE_URL)
+        wpars.get_html(wpars.BASE_URL)
         # Ensure correct url, called once
-        get_html.assert_called_once_with\
+        wpars.get_html.assert_called_once_with\
             ("https://www.weblancer.net/jobs/")
 
 
@@ -45,7 +45,91 @@ class TestParse(unittest.TestCase):
     @patch('webparser21.get_html', Mock())
     def test_process_page(self):
         wpars.get_html.return_value = self.html_doc
-        self.assertIsInstance(wpars.process_page(BASE_URL), list)
+        self.assertIsInstance(wpars.process_page(wpars.BASE_URL), list)
+
+    @patch('webparser21.get_html', Mock())
+    def test_parsing_all_page(self):
+        wpars.get_html.return_value = self.html_doc
+        mock_stdout = Mock()
+        sys.stdout = mock_stdout
+        data = wpars.parsing_all_page(wpars.BASE_URL)
+        self.assertEqual(len(data), 152*20)
+        self.assertEqual(data[2], data[22])
+
+    def tearDown(self):
+        del self.html_doc
+        del self.data_dict
+
+
+class TestSave(unittest.TestCase):
+    def setUp(self):
+        self.data_list = [{'title': u'Комплексное продвижение сайта (Игровая тематика: counter strike)',
+                           'category': u'Поисковые системы (SEO)',
+                           'price': u'',
+                           'applications': u'4 заявки'
+                           },
+                          {'title': u'Реферат по философии на тему "Специфика познания в медицине"',
+                           'category': u'Дипломы/Курсовые/Рефераты',
+                           'price': u'',
+                           'applications': u'исполнитель выбран'
+                           },
+                          {'title': u'Авторы учебных студенческих работ',
+                           'category': u'Дипломы/Курсовые/Рефераты',
+                           'price': '$1000',
+                           'applications': u'24 заявки'
+                           }]
+
+        self.data_dict = {'title': u'Авторы учебных студенческих работ',
+                          'category': u'Дипломы/Курсовые/Рефераты',
+                          'price': '$1000',
+                          'applications': u'24 заявки'
+                          }
+
+    def test_recode_for_write(self):
+        data = wpars.recode_for_write(wpars.NAME_COLUMM)
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 4)
+        self.assertEqual(data[1], '\xca\xe0\xf2\xe5\xe3\xee\xf0\xe8')
+
+    def test_recode_value_dict(self):
+        data = wpars.recode_value_dict(self.data_dict)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(len(data), 4)
+        self.assertEqual(data['applications'], '24 \xe7\xe0\xff\xe2\xea\xe8')
+
+    def test_save(self):
+        with patch('__builtin__.open', mock_open()) as mock_file:
+                   wpars.save(self.data_list, wpars.PATH_FILE)
+        mock_file.assert_any_call('proje.csv', 'w')
+        mock_file.assert_any_call('proje.csv', 'a')
+#         mock_file.write.assert_called_with('"sep=,"\n')
+#         self.assertEqual(mock_file.mock_calls[0], "call('proje.csv', 'w')")
+        mock_file.assert_any_call('proje.csv', 'w')
+#         mock_file.assert_any_call(write('"sep=,"\n'))
+
+        l = mock_file.mock_calls
+        print l
+        print mock_file.write()
+#         print mock_file.return_value
+#         d = mock_file.method_calls
+#         print d
+#         print l[2]
+#         self.assertEqual(l[2], """call().write('"sep=,"\\n')""")
+
+
+#     def test_save_2(self):
+#         m = mock_open()
+#         with patch('__builtin__.open', m, create=True):
+#             wpars.save(self.data_list, m)
+#
+#         print m.mock_calls
+
+
+#     @patch(('__builtin__.open', mock_open()) as mock_file)
+#     def test_save(self):
+#         wpars.save(self.data_list, mock_file)
+#         mock_file.assert_called_with('proje.csv')
+
 
 def main():
     unittest.main()
--- parser_example/webparser21.py
+++ parser_example/webparser21.py
@@ -1,10 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-import requests
+import requests, csv
 from bs4 import BeautifulSoup
 
 BASE_URL = "http://localhost/"
 # BASE_URL = "https://www.weblancer.net/jobs/"
+NAME_COLUMM = ['Проект', 'Категори', 'Цена', 'Заявки']
+PATH_FILE = 'proje.csv'
 
 def get_html_local():
     """
@@ -37,7 +39,7 @@ def parse(html_doc):
     """
     soup = BeautifulSoup(html_doc, 'html.parser')
     table = soup.find('div', class_="container-fluid cols_table show_visited")
-#    print table.prettify().encode('UTF-8')
+#     print table.prettify().encode('UTF-8')
     jobstats = []
 
     for row in table:
@@ -49,13 +51,12 @@ def parse(html_doc):
         })
     return jobstats
 
-
 def process_page(url):
     """
     Processing page
     """
-    html = get_html(url)
-#     html = get_html_local()
+#     html = get_html(url)
+    html = get_html_local()
     data = parse(html)
     return data
 
@@ -70,7 +71,7 @@ def parsing_all_page(url):
 
     projects = []
 
-    for page in range(1, page_count):
+    for page in range(1, page_count + 1):
         print 'Parsing %d%%' % (page*100/page_count)
 
         url = BASE_URL + '?page=%d' % page
@@ -78,17 +79,48 @@ def parsing_all_page(url):
 
     return projects
 
+def recode_for_write(sec):
+    """
+    Helper function for write dictonary values to windows code cp1251
+    """
+    sec2 = []
+    for i in sec:
+        sec2.append(i.decode('utf-8').encode('cp1251'))
+#         sec2.append(i.encode('cp1251'))
+    return sec2
+
+def recode_value_dict(dictionary):
+    """
+    Recursively recode value in dictionary for write in windows code
+    """
+    somedict = {k:v.encode('cp1251') for k, v in dictionary.items()}
+    return somedict
+
+def save(projects, path):
+    """
+    Write projects to excel csv file
+    """
+    with open(path,'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+        # string for help MS Excel
+        writer.writerow(['sep=,'])
+        # Table name_columm
+        writer.writerow(recode_for_write(NAME_COLUMM))
+
+    with open(path,'a') as csvfile:
+        headers = ['title', 'category', 'price', 'applications']
+        writer = csv.DictWriter(csvfile, lineterminator='\n',
+                                fieldnames=headers)
+        for i in projects:
+            i = recode_value_dict(i)
+            writer.writerow(i)
+
 
 def main():
-#     dar = process_page(BASE_URL)
-    dar = parsing_all_page(BASE_URL)
-    loc = dar[2]
-    print loc
-    print loc["title"]
-    print loc["category"]
-    print loc["price"]
-    print loc["applications"]
-    print dar[2]["price"]
+    data = process_page(BASE_URL)
+    save(data, PATH_FILE)
+
+    pass
 
 if __name__ == '__main__':
     main()
